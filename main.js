0349dc3c6e13ecaa0a9d100ff80e5ddc9676da54
unknown <rawankhalid557@gmail.com>
2024-12-16 15:26:36 +0200

    responsive error

--- main.js
+++ main.js
@@ -21,3 +21,68 @@ window.onclick = function(event) {
         closeModal();
     }
 };
+
+// const imageWrapper = document.querySelector('.image-wrapper')
+// const imageItems = document.querySelectorAll('.image-wrapper > *')
+// const imageLength = imageItems.length
+// const perView = 3
+// let totalScroll = 0
+// const delay = 2000
+
+// imageWrapper.style.setProperty('--per-view', perView)
+// for(let i = 0; i < perView; i++) {
+//   imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
+// }
+
+// let autoScroll = setInterval(scrolling, delay)
+
+// function scrolling() {
+//   totalScroll++
+//   if(totalScroll == imageLength + 1) {
+//     clearInterval(autoScroll)
+//     totalScroll = 1
+//     imageWrapper.style.transition = '0s'
+//     imageWrapper.style.left = '0'
+//     autoScroll = setInterval(scrolling, delay)
+//   }
+//   const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
+//   imageWrapper.style.left = `-${totalScroll * widthEl}px`
+//   imageWrapper.style.transition = '.3s'
+// }
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+// Auto Slide   if you need auto slide ,remove the commit "//"
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+   var slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) { slideIndex = 1 }
+   slides[slideIndex - 1].style.display = "block";
+   setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
